46919ad2824e152d928a24655f399a935a3b7799
Kelsey Fecho <kfecho30@gmail.com>
2018-06-19 14:12:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,9 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function deleteFromDriverByKey(driver, key) {
+
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
   
 }
