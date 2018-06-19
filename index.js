c75af126eeb76c28b95c79363dc328ccd5e9ba44
Kelsey Fecho <kfecho30@gmail.com>
2018-06-19 14:12:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ function updateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
-  return Object.assign(driver, {[key]: value})
+  return Object.assign(driver, {[key]: value});
+}
+
+function deleteFromDriverByKey(driver, key) {
+  
 }
