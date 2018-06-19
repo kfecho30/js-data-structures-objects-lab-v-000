6c5e775dfd3abb017bfa5c6f7333687185c48eb1
Kelsey Fecho <kfecho30@gmail.com>
2018-06-19 14:08:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 const driver = {}
 
 function updateDriverWithKeyAndValue(driver, key, value) {
-  Object.assign({} ,driver, key, value);
+  let new_driver.assign({} ,driver, key, value);
   return new_driver;
 }
